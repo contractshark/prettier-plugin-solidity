50548800d6671c844ba3de68fdb4fc49bd534df0
sam bacha <sam@manifoldfinance.com>
2021-09-09 20:13:20 -0700

    fuzz

---
+++ output/generated1.js
@@ -0,0 +1,95 @@
+'Ó';
+for (var async in this)
+	do break;
+	while (this instanceof false);
+for (
+	;
+	((0 !== null).qeru >>>= ([], q = true) => {
+		'VLÜ¡ò';
+		'J';
+		'	dÉ';
+		while (true) break;
+		async function async() {}
+		for (;;);
+	})`¢	Û${`${/|\B$(?:)|(?!)[^\--8-]Y{76003}?\4(?:)*?|\ud956\uDfDd??[^\t-h9-C\*-\^-]^\4|!+?\2()()()()/iuy}E${'B'}p`}Ù#ß${new (i
+		? this
+		: r.for)(
+		...async function (async, async, j) {
+			'use strict';
+		},
+		2e308,
+		async function emvpr(h, {}, async = null) {
+			'';
+		},
+	)}Nu(${false}a${() => {
+		'use strict';
+		switch ((async += 9926)) {
+			case this:
+			case null:
+			default:
+		}
+		function uru(r = 0x664888bcbc268, async, [], ...b) {
+			'w\
+';
+			'§';
+			'\N';
+			try {
+			} catch (l) {}
+		}
+		switch (null) {
+			case true:
+			default:
+			case false:
+		}
+	}}u${new null('àpiÙ'[2e308])}` + [,];
+
+)
+	throw (async (
+		async,
+		async = /9(?=)*(?=){85177,93994}/ ** async,
+		...{[0xc407fd521212e]: n = false, 'fêï': async, ...async}
+	) =>
+		function* (bqwoeqvnfpycks, t) {
+			'';
+			'use strict';
+			'&';
+			'use strict';
+			'use strict';
+		} <= [`Ôñ9`, 2e308 ? 2e308 : t, ,]).yfbbhsxhabb;
+async function async() {
+	'Â?á';
+	'use strict';
+	if (await (async = '¬Ù'))
+		for (null.constructor in async--)
+			try {
+				debugger;
+				var async;
+				try {
+				} finally {
+				}
+			} catch ({}) {
+			} finally {
+				('È¡');
+				while ('¶Ée\n');
+				for (;;) continue;
+			}
+	throw class eput {
+		'vÀ'() {}
+		static get [`Våo`]() {
+			debugger;
+			this;
+			try {
+			} finally {
+			}
+			for (_async of 0.87) debugger;
+		}
+		static async [new.target](e, d, x, async) {
+			"'Ä";
+			'';
+			'M';
+			'use strict';
+		}
+	};
+	for await (c of 0x1839f07ea90cba) var o, c, [] = `ÏÜ`;
+}
+import async, * as k from '';
